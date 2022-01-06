4182518c41b9538561a374e62f53fc51e39383a0
Alejandro Fermin <alejandro@debian-BULLSEYE-live-builder-AMD64>
2022-01-06 15:09:37 -0400

    Pequeño cambio en el script...

--- instalar.sh
+++ instalar.sh
@@ -169,6 +169,7 @@ function PICOM()
 	{
 			echo "#------------------------------Habilitar PICOM---------------------------------#"
 			sudo apt install gcc meson ninja-build python3 cmake pkg-config libpcre3 libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev -y
+			cd /tmp
 			git clone https://github.com/jonaburg/picom
 			cd picom
 			meson --buildtype=release . build
